0cffc51181f79d2d36fb339adf452593e6af7510
chrisb3546 <chrisb3546@yahoo.com>
2020-05-16 13:00:52 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name  FROM series
+  "SELECT authors.name, subgenres.name  FROM authors, subgenres
   INNER JOIN authors
   ON series
   WHERE series.author_id = authors.id "
